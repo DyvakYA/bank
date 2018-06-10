3f12403f2519a1124d85a5894cb15b655cdb8fe4
Dyvak Yurii <dyvakyurii@gmail.com>
2018-06-10 15:15:34 +0300

    add number generation

--- src/main/java/com/bank/honest/controller/TokenController.java
+++ src/main/java/com/bank/honest/controller/TokenController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,6 +43,7 @@ public class TokenController {
         if (!userService.findByPhone(authenticateDTO.getPhone()).getPassword().equals(authenticateDTO.getPassword())){
             throw new WrongPasswordException("Wrong password");
         } else{
+            SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
             CustomUser user = userService.findUserByPhone(authenticateDTO.getPhone());
             JWTUser jwtUser = JWTUser.builder()
                     .id(user.getId())
