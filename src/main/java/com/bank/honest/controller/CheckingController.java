4e57ff62db8b7e739051c866925d1acf181b2a35
Dyvak Yurii <dyvakyurii@gmail.com>
2018-12-01 22:38:11 +0200

    delete .idea folder

--- src/main/java/com/bank/honest/controller/CheckingController.java
+++ src/main/java/com/bank/honest/controller/CheckingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/checking")
-public class ChakingController {
+public class CheckingController {
 
     @Autowired
     private UserService userService;
